b405d265c3591f215668cc629d319a5c72024b3a
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-12-19 23:12:37 +0100

    component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const element = <h1>Hello world!</h1>;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(element);
+root.render(
+    <App/>
+);
