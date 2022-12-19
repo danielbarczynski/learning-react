82eafa902a02023242bb67c450853a568e061bdb
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-12-19 23:24:30 +0100

    function name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function App(){
+    const handleNameChange = () => {
+        const names = ['Nathan', 'Elizabeth', 'Marcus', 'Kayla']
+        let rand = Math.floor(Math.random() * 4);
+        return names[rand];
+    }
+
     return (
-        <h1>Hello world!</h1>
+        <h1>Hello { handleNameChange() }</h1>
     )
 }
 
