942ed6060b2c8a28220de2abc2958876324c0955
Daniel PC <daniel.barczynski@onet.pl>
2022-12-20 12:52:05 +0100

    useState

--- src/Content.js
+++ src/Content.js
@@ -1,25 +1,27 @@
 import React from 'react';
 
 const Content = () => {
+    const [count, setCount] = React.useState(0); // if all React imported. 0 is default int value for my counter
+    // const [count, setCount] = useState(0); // if all just {useState} imported
+
     const handleNameChange = () => {
         const names = ['Nathan', 'Elizabeth', 'Marcus', 'Kayla']
         let rand = Math.floor(Math.random() * 4);
         return names[rand];
     }
 
-    const handleClickEvent = () => console.log('You clicked me');
-    const handleClickEvent2 = (name) => console.log(name);
-    const handleClickEvent3 = (e) => console.log(e.target);
-    
+    const handleClickEvent = () => {
+        // count++; // can't because constant
+        // setCount(count++); // it is also assigment to const 
+        setCount(count + 1); // this is fine
+        console.log(count) //* it also changes handleNameChange (from react virtual DOM)
+    };
+  
     return (
         // styling same as in index.css just for learning purposes
         <main style={{fontSize: '22px', color: 'aliceblue'}}>
             Hello {handleNameChange()}
             <button onClick={handleClickEvent}>Click me</button>
-            {/* this will be automatically invoked at start - bad practice */}
-            {/* <button onClick={handleClickEvent2('Daniel')}>Click me</button>  */}
-            <button onClick={() => handleClickEvent2('Daniel')}>Click me</button> 
-            <button onClick={(e) => handleClickEvent3(e)}>Click me</button>
         </main>
     )
 };
