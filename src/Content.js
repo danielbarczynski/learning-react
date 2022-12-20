229c29fe28e9d83b14e7e485d452316fa78328a5
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-12-20 11:36:23 +0100

    events

--- src/Content.js
+++ src/Content.js
@@ -7,10 +7,19 @@ const Content = () => {
         return names[rand];
     }
 
+    const handleClickEvent = () => console.log('You clicked me');
+    const handleClickEvent2 = (name) => console.log(name);
+    const handleClickEvent3 = (e) => console.log(e.target);
+    
     return (
         // styling same as in index.css just for learning purposes
         <main style={{fontSize: '22px', color: 'aliceblue'}}>
             Hello {handleNameChange()}
+            <button onClick={handleClickEvent}>Click me</button>
+            {/* this will be automatically invoked at start - bad practice */}
+            {/* <button onClick={handleClickEvent2('Daniel')}>Click me</button>  */}
+            <button onClick={() => handleClickEvent2('Daniel')}>Click me</button> 
+            <button onClick={(e) => handleClickEvent3(e)}>Click me</button>
         </main>
     )
 };
