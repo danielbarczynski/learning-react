ad6d72b4a22e6fe8c8891f0f226dce5ee5e8728c
Daniel Barczyński <daniel.barczynski@onet.pl>
2022-12-21 19:34:21 +0100

    fetch CRUD

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Header from "./Header";
 import SearchItem from "./SearchItem";
+import apiRequest from './apiRequest';
 import "./index.css";
-
 
 function App() {
     const API_URL = 'http://localhost:3500/items';
@@ -19,10 +19,8 @@ function App() {
         const fetchData = async () => {
             try {
                 const response = await fetch(API_URL);
-
-                if (!response.ok) 
+                if (!response.ok)
                     throw Error('Did not receive expected data');
-
                 const dbItems = await response.json();
                 setItems(dbItems);
 
@@ -32,28 +30,40 @@ function App() {
                 setIsLoading(false);
             }
         }
-        
-        // (async () => await fetchData())();
         setTimeout(() => {
             fetchData();
         }, 2000);
     }, []);
 
-   
-
-    const handleCheck = (id) => {
+    const handleCheck = async (id) => {
         const newItems = items.map((item) => {
             if (item.id === id)
                 item.checked = !item.checked;
             return item;
         });
-
         setItems(newItems);
+        const newItem = items.filter((item) => item.id === id);
+        const updateOptions = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ checked: newItem[0].checked })
+        };
+        const reqUrl = `${API_URL}/${id}`;
+        const result = await apiRequest(reqUrl, updateOptions);
+        if (result) 
+            setCatchError(result);
     };
 
-    const handleDelete = (id) => {
+    const handleDelete = async (id) => {
         const newItems = items.filter((item) => item.id !== id);
         setItems(newItems);
+        const deleteOptions = { method: 'DELETE' };
+        const reqUrl = `${API_URL}/${id}`;
+        const result = await apiRequest(reqUrl, deleteOptions);
+        if (result) 
+            setCatchError(result);
     };
 
     const handleSubmit = (e) => {
@@ -62,15 +72,24 @@ function App() {
         setInputItem('');
     };
 
-    const addItem = () => {
+    const addItem = async () => {
         const newItem = {
             id: items.length ? items[items.length - 1].id + 1 : 1,
             checked: false,
             description: `${inputItem}`
         }
-        const newItems = [...items, newItem]; // uses useEffect
-        // items.push(newItem); // not uses useEffect
-        setItems(newItems)
+        const newItems = [...items, newItem];
+        setItems(newItems);
+        const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newItem)
+        }
+        const result = await apiRequest(API_URL, postOptions);
+        if (result) 
+            setCatchError(result);
     }
 
     return (
